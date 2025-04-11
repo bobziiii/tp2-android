388e35a6373ed4a2ed4575af75d19cc5dd65a73a
bobziiii <felix.boyer.1@ens.etsmtl.ca>
2025-04-11 11:05:45 -0400

    manque 1 xml

--- app/src/main/java/com/example/comfelix_nath_anprojetx/Historique.java
+++ app/src/main/java/com/example/comfelix_nath_anprojetx/Historique.java
@@ -18,7 +18,7 @@ public class Historique extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;z
+            return insets;
         });
     }
 }
